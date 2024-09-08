47cd9c6ed688ce601c287b8912c90a00581ff0c3
Sidhantha <sidhanthapoddar99@gmail.com>
2024-09-08 07:07:02 +0530

    Refactor user_pairs, balance, and build_transactions in __init__.py

--- Flask_backend/app/__init__.py
+++ Flask_backend/app/__init__.py
@@ -14,23 +14,44 @@ app.config["Atlas_data"] = info['atlas_data']
 
 app.config["Item_data"] = json.load(open(r'resources\detial_items.json'))
 app.config["reverse_map"] = json.load(open(r'resources\reverse_map.json'))
-app.config["user_pairs"] = {"abc123": "6Senu4mrPDM1Mb1xJYFXSDg2CggWkFLC9VCNzp1LEHBX",
-                            "abc1234": "Ad8yNpE3seJKHoNstbDHjG5F5ZCapapuxtpKKMWPGML5"}
-app.config["balance"] = {'6Senu4mrPDM1Mb1xJYFXSDg2CggWkFLC9VCNzp1LEHBX': 100}
-
-TRANSACTION_PARAMS = {
-    'orderCreator': '6Senu4mrPDM1Mb1xJYFXSDg2CggWkFLC9VCNzp1LEHBX',
-    'itemMint': 'Ev3xUhc1Leqi4qR2E5VoG9pcxCvHHmnAaSRVPg485xAT',
-    'quoteMint': 'ATLASXmbPQxBUYbxPsV97usA3fPQYEqzQBUHgiFCUsXx',
-    'quantity': '1',
-    'uiPrice': '10.5',
-    'programId': 'traderDnaR5w6Tcoi3NFm53i48FTDNbGjBSZwWXDRrg',
-    'orderSide': 'sell'
-}
 
 
-app.config["build_transactions"] = { "abc": TRANSACTION_PARAMS }
+
+
+
+
+
+app.config["user_pairs"] = {}
+app.config["balance"] = {}
+app.config["build_transactions"] = {}
 app.config['instructions'] = get_instruction_dictionary()
+
+
+
+
+
+
+
+
+
+# Sample Transactions
+# app.config["user_pairs"] = {"abc123": "6Senu4mrPDM1Mb1xJYFXSDg2CggWkFLC9VCNzp1LEHBX",
+#                             "abc1234": "Ad8yNpE3seJKHoNstbDHjG5F5ZCapapuxtpKKMWPGML5"}
+# app.config["balance"] = {'6Senu4mrPDM1Mb1xJYFXSDg2CggWkFLC9VCNzp1LEHBX': 100}
+
+# TRANSACTION_PARAMS = {
+#     'orderCreator': '6Senu4mrPDM1Mb1xJYFXSDg2CggWkFLC9VCNzp1LEHBX',
+#     'itemMint': 'Ev3xUhc1Leqi4qR2E5VoG9pcxCvHHmnAaSRVPg485xAT',
+#     'quoteMint': 'ATLASXmbPQxBUYbxPsV97usA3fPQYEqzQBUHgiFCUsXx',
+#     'quantity': '1',
+#     'uiPrice': '10.5',
+#     'programId': 'traderDnaR5w6Tcoi3NFm53i48FTDNbGjBSZwWXDRrg',
+#     'orderSide': 'sell'
+# }
+
+
+# app.config["build_transactions"] = { "abc": TRANSACTION_PARAMS }
+
 
 
 
